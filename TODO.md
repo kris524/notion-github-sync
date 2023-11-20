b19644518d438a67eb5834801acac2a4c9a63c0e
Kris <kristian_dilov@abv.bg>
2023-11-20 23:57:56 +0000

    capture issue data from notion

---
+++ TODO.md
@@ -0,0 +1,17 @@
+Will couple the Notion page called Bug tracking from the Notion templates to Github issues
+Essentially each new page created there will be made into an issue in Github with the appropriate label
+It is important we keep everything as it curretly is so everyone has the same setup
+
+For example:
+
+Rejected -> wont fix 
+To reveiw -> no label
+Validating -> no label
+Tasks backlog -> no label
+
+In the descrption include: Reported by: Name
+
+Will not assing to anyone 
+
+Type, if its Improvement or Feature -> enhancement
+      if its Issue -> bug 
--- src/sync_notion.mjs
+++ src/sync_notion.mjs
@@ -9,23 +9,60 @@ const notion = new Client({auth: process.env.NOTION_API})
 async function get_connected_pages(){
     const pages = await notion.search({})
 
-    console.log("Users Notion:", pages["results"])
+    // console.log("Users Notion:", pages["results"])
     return pages["results"]
 }
 
-// const pages = get_connected_pages()
+const pages = await get_connected_pages()
+
+console.log(pages)
+
+var list_of_issues = []
+
+pages.forEach( (dict) =>{
+
+    var issue = {}
+
+    if (dict.parent.type === "database_id") 
+    {
+       console.log(dict.properties["Type"].select.name) 
+        
+       issue["title"] = dict.properties["Name"].title[0].plain_text
+       issue["labels"] = dict.properties["Type"].select.name
+    //    issue["body"]
+       
+       list_of_issues.push(issue)
+    }
+
+}) 
+
+console.log(list_of_issues)
+
 
 
 async function get_connected_database(){
     const pages = await notion.search({})
 
     const notion_database = pages["results"].filter(dict => dict["object"] === "database")
-    console.log(notion_database)
+    return notion_database[0]
 
 }
 
+// We want the data only from pages that have the database_id
 
-get_connected_database()
+
+// const ndb = await get_connected_database()
+
+// console.log(ndb)
+
+// Object.entries(ndb.properties).forEach( ([property_name, property_value]) => {
+
+//     console.log(property_name)
+//     console.log(property_value)
+
+// })
+
+
 
 // async function get_db_properties()
 
