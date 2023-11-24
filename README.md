e527fdab3ae0000aafc14b4da8bcc0046f2f0ff2
Kris <kristian_dilov@abv.bg>
2023-11-24 23:58:32 +0000

    get_paragraphs

---
+++ README.md
@@ -0,0 +1,22 @@
+# Notion-GitHub Sync Integration
+
+This integration serves as a seamless bridge between Notion and GitHub, facilitating the efficient tracking of issues, bugs, and features for your projects.
+
+## Overview
+
+The integration is tailored to work seamlessly with a dedicated Notion template designed specifically for bug tracking. It automates the synchronization of information between Notion and GitHub issues, streamlining the management process and ensuring consistency across platforms.
+
+## Key Features
+
+- **Bidirectional Sync:** Keep your Notion workspace and GitHub issues in sync effortlessly. Changes made in either platform reflect seamlessly in the other.
+
+- **Template Compatibility:** The integration is optimized for use with a Notion template crafted to capture and manage bug-related information effectively.
+
+- **Efficient Collaboration:** Enhance team collaboration by centralizing issue tracking in Notion while seamlessly integrating with GitHub's powerful issue management capabilities.
+
+## Getting Started
+
+Follow these simple steps to integrate Notion and GitHub:
+
+<!-- 1. **Clone the Repository:**
+   Clone this repository to your local environment. -->
--- src/sync_notion.mjs
+++ src/sync_notion.mjs
@@ -9,7 +9,6 @@ const notion = new Client({auth: process.env.NOTION_API})
 async function get_connected_pages(){
     const pages = await notion.search({})
 
-    // console.log("Users Notion:", pages["results"])
     return pages["results"]
 }
 
@@ -17,58 +16,68 @@ const pages = await get_connected_pages()
 
 // console.log(pages)
 
-var list_of_issues = []
 
-pages.forEach( (dict) =>{
 
-    var issue = {}
-    
-    // condition to get only pages part of the database, excluding the template page built-in the database 
-    if (dict.parent.type === "database_id" && dict.properties["Type"].select !== null) 
-    {
+
+async function build_issue_data(pages) {
+
+    var list_of_issues = []
+
+    pages.forEach( async (dict) =>{
+
+        var issue = {}
         
-       issue["title"] = dict.properties["Name"].title[0].plain_text
-       issue["labels"] = dict.properties["Type"].select.name
-       var page_id = dict["id"]
-    //    issue["body"]
-       
-       list_of_issues.push(issue)
-    }
+        // condition to get only pages part of the database, excluding the template page built-in the database 
+        if (dict.parent.type === "database_id" && dict.properties["Type"].select !== null) 
+        {
+            
+        issue["title"] = dict.properties["Name"].title[0].plain_text
+        issue["labels"] = dict.properties["Type"].select.name
 
-}) 
+        const paragraphs = await get_paragraphs(dict["id"])
+        //    console.log(paragraphs)
+        issue["body"] = paragraphs
+        // console.log(issue)
+        list_of_issues.push(issue)
+
+            }
+
+        }) 
+    
+        return list_of_issues;
+
+}
+
+const list_of_issues = await build_issue_data(pages);
 
 console.log(list_of_issues);
 
-// NEXT STEPS
-// Do not delete the template in Notion
-// For each page (excluding the template) get the title, label and text
-// to get the text, we need to use the id to get the blocks in the page, and get the text from those blocks
-
-
-
 async function get_paragraphs(blockId) {
-//"fde64ac0-06e2-4b3a-9db6-126d75049c4d";
   const response = await notion.blocks.children.list({
     block_id: blockId,
     page_size: 50,
   });
 
-//   console.log(response["results"]);
-  return response["results"]
+  var paragraphs = ""
+
+  const res = response["results"]
+
+  res.forEach( (dict) => {
+    
+    if (dict["type"] == "paragraph"){
+        // console.log(dict.paragraph.rich_text) 
+        const data = dict.paragraph.rich_text
+        const plainText = data.map(item => item.plain_text)
+        
+        paragraphs += plainText
+        } 
+    }
+    )
+    return paragraphs;
 };
 
-const res = await get_paragraphs("fde64ac0-06e2-4b3a-9db6-126d75049c4d")
-
-var paragraphs = ""
-res.forEach( (dict) => {
-    if (dict["type"] == "paragraph"){
-        console.log(dict["paragraph"]["rich_text"])
-    } 
-}
-
-
-
-)
+// const paragraphs = await get_paragraphs("fde64ac0-06e2-4b3a-9db6-126d75049c4d")
+// console.log(paragraphs)
 
 
 // async function get_connected_database(){
